993632b2a89a90f4251aa4a5d631c6fbb814596c
Tiago Luiz <56927985+MajorNameless@users.noreply.github.com>
2024-11-03 16:25:28 +0200

    Refactored BusinessLogicLayer for .NET

---
+++ BusinessLogicLayer/Driver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StartSmartDeliveryForm.DataLayer.DAOs;
+using StartSmartDeliveryForm.SharedLayer.Enums;
+using StartSmartDeliveryForm.SharedLayer;
+
+namespace StartSmartDeliveryForm.BusinessLogicLayer
+{
+    internal class Driver
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EmployeeNo { get; set; }
+        public LicenseType LicenseType { get; set; }
+        public bool Availability { get; set; }
+
+        public static bool IsEmployeeNoUnique(string EmployeeNo)
+        {
+            int Count = DriversDAO.GetEmployeeNoCount(EmployeeNo);
+            bool IsUnique = Count == 0;
+            FormConsole.Instance.Log($"Employee Unique: {IsUnique}");
+            return IsUnique;
+        }
+
+    }
+}
