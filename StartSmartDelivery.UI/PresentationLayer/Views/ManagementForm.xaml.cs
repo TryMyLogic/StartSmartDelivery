f576a95d280ff9918e5e8335c24eaeeeef4bc4ea
Tiago Luiz <56927985+MajorNameless@users.noreply.github.com>
2025-11-24 08:52:43 +0200

    feat(ui): add WPF views in new project
    - Mirrors existing WinForms UI with minor top section changes
    - Rewrite chosen due to time elapsed, allowing improvements not previously possible in areas such as printing and applying lessons learned

---
+++ StartSmartDelivery.UI/PresentationLayer/Views/ManagementForm.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StartSmartDelivery.UI
+{
+    /// <summary>
+    /// Interaction logic for ManagementForm.xaml
+    /// </summary>
+    public partial class ManagementForm : Window
+    {
+        public ManagementForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ StartSmartDelivery.UI/PresentationLayer/Views/ManagementTopBar.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StartSmartDelivery.UI.PresentationLayer.Views
+{
+    /// <summary>
+    /// Interaction logic for ManagementTopBar.xaml
+    /// </summary>
+    public partial class ManagementTopBar : UserControl
+    {
+        public ManagementTopBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
